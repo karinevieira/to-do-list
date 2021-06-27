7be692ddeac0020542d61042f5d1156ce0ce0f5c
Karine Vieira <kv.vieira1@gmail.com>
2021-06-27 09:38:50 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 let input = document.querySelector('#tarefa')
 let lista = document.querySelector('#lista')
-let completar = `<img src="complete.png" alt="complete" class="task-image">`
+let completar = `<img src="complete.png" alt="complete" class="task-image" onclick="remover()">`
 let deletar =  `<img src="delete.png" alt="delete" class="task-image">`
 
 function adicionar(){
+    lista.style.display = 'block'
     lista.innerHTML += `<li>${input.value}</li>`
     lista.innerHTML += completar
     lista.innerHTML += deletar
+}
+
+function remover(){
+    lista.style.display = 'none'
 }
 
